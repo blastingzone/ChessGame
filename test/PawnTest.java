be3c17ad42af5d3c946f21a87646d28b1a1471fa
Moonjinsang <blastingzone@naver.com>
2013-07-12 15:48:25 +0900

    연습문제 1-5

--- test/PawnTest.java
+++ test/PawnTest.java
@@ -5,5 +5,9 @@ public class PawnTest extends TestCase {
 		Pawn cutiePawn = new Pawn("White");
 		String pawnColor = cutiePawn.getColor();
 		assertEquals(pawnColor, "White");
+		
+		Pawn prettyPawn = new Pawn("Black");
+		pawnColor = prettyPawn.getColor();
+		assertEquals("Black", pawnColor);
 	}
 }
