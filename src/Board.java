96039f7fec712a7db3f47a69f9b5d337fc05a5ab
Moonjinsang <blastingzone@naver.com>
2013-07-17 21:40:35 +0900

    연습문제 2-5

--- src/Board.java
+++ src/Board.java
@@ -8,14 +8,22 @@ public class Board {
 	}
 
 	int pieceNumber() {
-
 		return board.size();
 	}
 
+	/**
+	 * 체스판에 Pawn을 추가시키는 함수이다.
+	 * @param pawn : 체스에 사용할 Pawn Class이다. Black / White Color값을 가질 수 있다.
+	 */
 	void addPiece(Pawn pawn) {
 		board.add(pawn);
 	}
 	
+	/**
+	 * 
+	 * @param pawn : Pawn Class의 Instance
+	 * @return 해당 pawn이 있으면 true, 없으면 false
+	 */
 	boolean contain(Pawn pawn) {
 		return board.contains(pawn);
 	}
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -8,13 +8,20 @@ public class BoardTest extends TestCase {
 		assertEquals(0, board.pieceNumber());
 	}
 	
+	/**
+	 * 체스판에 말을 추가하고 확인하는 테스트 함
+	 */
 	public void testAddPiece() throws Exception {
+		/**
+		 * Pawn() == Pawn("White");
+		 */
 		Pawn whitePawn1 = new Pawn(); 
 		Pawn blackPawn1 = new Pawn("Black");
 		
 		board.addPiece(whitePawn1);
 		assertEquals(1, board.pieceNumber());
 		assertEquals(true, board.contain(whitePawn1));
+		
 		board.addPiece(blackPawn1);
 		assertEquals(2, board.pieceNumber());
 		assertEquals(true, board.contain(blackPawn1));
